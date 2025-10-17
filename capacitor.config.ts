bd05800029a5d8a6009b6fbd1d623ef08af9bed6
François Rossigneux <francois.rossigneux@gmail.com>
2025-10-17 23:57:15 +0200

    Fix build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.lia.app',
+  appId: 'pro.lia.app',
   appName: 'Lia',
   webDir: 'dist',
   bundledWebRuntime: false,
